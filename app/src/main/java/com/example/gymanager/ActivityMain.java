254eae992e07ba07bc57da0ef53b07909b55b7fb
ydd1337 <steamac2@gmail.com>
2018-02-15 01:28:00 +0200

    ActivityMain

---
+++ app/src/main/java/com/example/gymanager/ActivityMain.java
@@ -0,0 +1,14 @@
+package com.example.gymanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class ActivityMain extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
