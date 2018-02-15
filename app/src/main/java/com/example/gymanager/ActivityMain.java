f281d62b082062b0ac9d375ff45505a8766202b9
ydd1337 <steamac2@gmail.com>
2018-02-15 15:12:13 +0200

    ToolBar

--- app/src/main/java/com/example/gymanager/ActivityMain.java
+++ app/src/main/java/com/example/gymanager/ActivityMain.java
@@ -1,14 +1,33 @@
 package com.example.gymanager;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 public class ActivityMain extends Activity {
+    private android.support.v7.widget.Toolbar toolbar;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+    }
+
+
+    private void initToolbar() {
+        toolbar = (android.support.v7.widget.Toolbar)findViewById(R.id.ToolBar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new android.support.v7.widget.Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
